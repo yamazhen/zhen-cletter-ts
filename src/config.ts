8972467819526e11bf69b2d710287612fa3b48e6
yamazhen <bowxyt@gmail.com>
2025-09-13 16:27:23 +0900

    use system env

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,6 @@
-import dotenv from "dotenv";
-dotenv.config({ quiet: true });
-
 const GEMINI_KEY = process.env["GEMINI_API"];
 if (!GEMINI_KEY) {
-	throw Error("Gemini key not set");
+	throw Error("GEMINI_API env var unavailable");
 }
 
 export { GEMINI_KEY };
